0b1592abb58f393fbd3044ff7461fc7542f7dd77
rustam <Rusik2006@gmail.com>
2022-10-14 15:19:35 +0600

    added month-5_hw-2

--- movie_app/serializers.py
+++ movie_app/serializers.py
@@ -3,18 +3,66 @@ from .models import Director, Movie, Review
 
 
 class DirectorListSerializers(serializers.ModelSerializer):
+    movies_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Director
-        fields = 'id name'.split()
+        fields = 'id name movies_count'.split()
+
+    def get_movies_count(self, obj_director):
+        return obj_director.movies.count()
+
+
+class DirectorSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Director
+        fields = 'name'.split()
 
 
 class MovieListSerializers(serializers.ModelSerializer):
+    director = DirectorSerializers()
+
     class Meta:
         model = Movie
-        fields = 'id title'.split()
+        fields = 'title description duration director director_name'.split()
+
+
+class MovieSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = 'title'.split()
 
 
 class ReviewListSerializers(serializers.ModelSerializer):
+    movie = MovieSerializers()
+
     class Meta:
         model = Review
-        fields = 'id text'.split()
+        fields = 'text movie movie_title stars'.split()
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = 'id movie text stars'.split()
+
+    def to_representation(self, instance):
+        show = super().to_representation(instance)
+        show['movie'] = instance.movie.title
+
+        return show
+
+
+class MoviesReviewsListSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True)
+    rating = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Movie
+        fields = 'title reviews rating'.split()
+
+    def get_rating(self, obj_movie):
+        sum_ = 0
+        for i in obj_movie.reviews.all():
+            sum_ += int(i.stars)
+        return round(sum_ / obj_movie.reviews.count(), 1) if obj_movie.reviews.count() else "This movie has no rating"
--- movie_app/views.py
+++ movie_app/views.py
@@ -1,7 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import DirectorListSerializers, MovieListSerializers, ReviewListSerializers
 from .models import Director, Movie, Review
+from rest_framework import status
+from .serializers import DirectorListSerializers, MovieListSerializers, ReviewListSerializers, \
+    MoviesReviewsListSerializer
 
 
 @api_view(['GET'])
@@ -23,3 +25,41 @@ def reviews_view(request):
     reviews = Review.objects.all()
     data = ReviewListSerializers(reviews, many=True).data
     return Response(data=data)
+
+
+@api_view(['GET'])
+def movies_reviews_view(request):
+    movies_reviews = Movie.objects.all()
+    data = MoviesReviewsListSerializer(movies_reviews, many=True).data
+    return Response(data=data)
+
+
+@api_view(['GET'])
+def director_item_view(reques, id):
+    try:
+        director = Director.objects.get(id=id)
+    except Director.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND, data={'error': 'Director not found'})
+    serializer = DirectorListSerializers(director)
+    return Response(data=serializer.data)
+
+
+@api_view(['GET'])
+def movie_item_view(reques, id):
+    try:
+        movie = Movie.objects.get(id=id)
+    except Movie.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND, data={'error': 'Movie not found'})
+    serializer = MovieListSerializers(movie)
+    return Response(data=serializer.data)
+
+
+@api_view(['GET'])
+def review_item_view(reques, id):
+    try:
+        movie = Review.objects.get(id=id)
+    except Review.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND, data={'error': 'Review not found'})
+    serializer = ReviewListSerializers(movie)
+    return Response(data=serializer.data)
+
